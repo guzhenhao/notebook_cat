4e0ccede095d4668557f3fba46d38189c94cbebd
guzhenhao <41565316+guzhenhao@users.noreply.github.com>
2018-08-23 18:08:19 +0800

    complete JSON  write

---
+++ cat.py
@@ -0,0 +1,25 @@
+import json
+
+notebook1 = open('1.ipynb')
+notebook1_str = notebook1.read()
+
+notebook1_json =json.loads(notebook1_str)
+
+
+cells1 = notebook1_json['cells']
+
+notebook2 = open('2.ipynb')
+notebook2_str = notebook2.read()
+
+notebook2_json =json.loads(notebook2_str)
+
+cells2 = notebook2_json['cells']
+
+
+target_cells = cells1 + cells2
+
+target_json = json.dumps(target_cells)
+
+target = open('target_notebook.ipynb','w')
+
+target.write(target_json)
